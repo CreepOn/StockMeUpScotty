c42a443f0e446cfffe6b0a5e0b7ae9a165be603f
a-greve <a-greve@users.noreply.github.com>
2016-05-30 16:58:26 +0200

    Added JsonToList and CandleStickPlotting

--- loadDataFunctions.py
+++ loadDataFunctions.py
@@ -1,10 +1,46 @@
 from yahoo_finance import Share
 from datetime import *
 import json
+from pylab import *
+import matplotlib.pyplot as plt
+import time
+from matplotlib.dates import  DateFormatter, WeekdayLocator, HourLocator, \
+     DayLocator, MONDAY
+from matplotlib.finance import candlestick,\
+     plot_day_summary, candlestick2
 
 whiteFont="\033[0m"
 greenFont="\033[92m"
 redFont="\033[91m"
+
+def JsonToList(hist):
+	data = []
+	for i in hist:
+		Date1 = date2num(datetime.strptime(i['Date'], "%Y-%m-%d"))
+		data.append((Date1,float(i['Open']),float(i['Close']),float(i['High']),float(i['Low'])))
+	return data
+
+def CandleStickPlotting(plotdata):
+	mondays = WeekdayLocator(MONDAY)        # major ticks on the mondays
+	alldays    = DayLocator()              # minor ticks on the days
+	weekFormatter = DateFormatter('%b %d')  # e.g., Jan 12
+	dayFormatter = DateFormatter('%d')      # e.g., 12
+
+	title = 'Candlestick diagram'
+	#and then following the official example. 
+	fig, ax = plt.subplots()
+	fig.subplots_adjust(bottom=0.2)
+	ax.xaxis.set_major_locator(mondays)
+	ax.xaxis.set_minor_locator(alldays)
+	ax.xaxis.set_major_formatter(weekFormatter)
+	candlestick(ax, plotdata, width=0.6)
+
+	ax.xaxis_date()
+	ax.autoscale_view()
+	fig.suptitle(title, fontsize=12)
+	plt.setp( plt.gca().get_xticklabels(), rotation=45, horizontalalignment='right')
+
+	savefig('foo.pdf')
 
 def today():
 	datotid=datetime.now()
@@ -67,6 +103,7 @@ def printRawData(data):
 			print whiteFont
 		except ValueError:
 			print "NONONO"
+
 
 #get_price()
 #get_change()
