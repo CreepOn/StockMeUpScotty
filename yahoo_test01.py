7bc99e7bb1dc20d98e259cdcc7f25a3b49adc3c7
CreepOn <emil_keinicke@hotmail.com>
2016-05-30 19:30:41 +0200

    Including the plots.py for plotting definitions

--- yahoo_test01.py
+++ yahoo_test01.py
@@ -2,16 +2,22 @@ import os
 from markets import *
 import json
 from loadDataFunctions import *
+import numpy as np
+from stockIndicators import *
 os.system('clear')
 
-tickers=get_C20()
-print len(tickers)
+#tickers=get_C20()
 #for i in range(0,len(tickers)):
 #	data=getHistoricalData(tickers[i], 4)
 #	printRawData(data)
 	
-data=getHistoricalData('TSLA', 7)
+data=getHistoricalData('TSLA', 365)
 
-print data[0]['Volume']
+listdata=matrix(JsonToList(data))
 
+#print listdata[0:,0]
+sma=movingAverage(map(float, listdata[0:,1]),10)
 
+print "list: " + str(len(listdata)) + "  SMA: " + str(len(sma))
+
+#CandleStickPlotting(listdata)
