e98333e2dacdd31af3d1afc794b946e3ac530e1a
CreepOn <emil_keinicke@hotmail.com>
2016-08-22 20:12:47 +0200

    Test for Anders

--- test01.py
+++ test01.py
@@ -15,20 +15,21 @@ redFont="\033[91m"
 
 #os.system('clear')
 
-ticker='TSLA'
+ticker=['TSLA','NOVO-B.CO']
 
-ld=getDataObject(ticker,100)
+for ele in ticker:
+	ld=getDataObject(ele,100)
 
-print ld
+	print ld
 
-soo=SO(ld, 14, 3)
+	soo=SO(ld, 14, 3)
 
-rsii=RSI(ld, 14)
+	rsii=RSI(ld, 14)
 
-smaa1=SMA(ld,2)
+	smaa1=SMA(ld,2)
 
 
-Plot(rsii, soo, smaa1)
+	Plot(rsii, soo, smaa1)
 
 #print SO(ld, 14,3).data[:,1:]
 print "end"
